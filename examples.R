da3aa34de04fa8f5c1bcf4dcbdf419491071c305
bchasco <brandon.chasco@gmail.com>
2025-06-23 22:17:21 -0700

    Just loop over S2 and D2.

--- examples.R
+++ examples.R
@@ -8,7 +8,7 @@ df <- data.frame(
 )
 
 formulas <- list(
-  phi = ~ -1 + f_yr + f_tk + (1|f_site),
+  phi = ~ 1 + (1|f_tl) + f_site + f_tk,
   p   = ~ 1
 )
 
@@ -23,34 +23,37 @@ params    <- make_param_vectors(design, c("phi"))
 beta_vals <- seq_along(params$beta$phi)
 names(beta_vals) <- names(params$beta$phi)
 # random effects get 100,200,300…
-u_vals    <- seq_along(params$u$phi) * 100
-names(u_vals)   <- names(params$u)
+if(ncol(lk$phi$Z_min)>0){
+  u_vals    <- seq_along(params$u$phi) * 100
+} else {
+  u_vals <- NA
+}
 
-
-# (1) precompute the LP for each minimal‐design row
-eta_min <- as.numeric(
-  (lk$phi$X_min) %*% beta_vals +
-     lk$phi$Z_min %*% u_vals
-)
-eta_row <- as.numeric(
-  rownames(lk$phi$X_min) #%*% beta_vals +
-  # design$Z_group$phi #%*% u_vals
-)
-
-# dims
 G <- max(lk$phi$lookup$g)
-S <- max(lk$phi$lookup$state) + 2  # +2 for the “dead” and “unseen” states
+S <- if (!is.null(design$state)) nlevels(df$f_tl) + 2 else 3L
 T <- max(lk$phi$lookup$time)
-S2 <- array(0, c(G,S,S,T))
-Sidx <- array(0, c(G,S,S,T))
 
-# eta_min <- result$phi$X_min %*% beta_vals    # + result$phi$Z_min %*% u_vals
-idx     <- cbind(lk$phi$lookup$g, lk$phi$lookup$state, lk$phi$lookup$state, lk$phi$lookup$time)
-S2[idx] <- eta_min[ lk$phi$lookup$design_row ]
-Sidx[idx] <- eta_row[ lk$phi$lookup$design_row ]
-S2[1,1,,]
-Sidx[1,1,,]
+S2 <- array(0, c(G, S, S, T))
+
+
+for(i in 1:nrow(lk$phi$lookup)){
+  print(i)
+  row <- lk$phi$lookup[i,]
+  beta_i <- lk$phi$X_min[row$design_row,] %*% beta_vals
+  if(ncol(lk$phi$Z_min)>0){
+    re_i <- lk$phi$Z_min[row$re_row,] %*% u_vals
+    beta_i <- beta_i + re_i
+  } else {
+    re_i <- 0
+  }
+  S2[row$g,row$state,row$state,row$time] <- beta_i
+}
+S2[1,1,,1]
+S2[1,,,1]
+
 S2[1,1,,]
 S2[1,S-2,,]
 S2[G,S-2,,]
+S2[1,,,1]
 print(dim(S2))
+table(S2)
